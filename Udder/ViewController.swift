09648b354f5e04fb2a194facc74423be03bf85e0
Allan King <allanking75@gmail.com>
2021-08-30 13:34:35 +0200

    add code to didreceivememorywarning

--- Udder/ViewController.swift
+++ Udder/ViewController.swift
@@ -15,7 +15,7 @@ class ViewController: UIViewController {
     }
     
     override func didReceiveMemoryWarning() {
-        //code here
+    //code here 9i =-p j=[a]\ DFXb/;p00-'['qws[dc-v -[c d0c9 8 jkslR:<Tikp[j-0hnvc9x0x;o/K]=
     }
     
     override func viewWillAppear(_ animated: Bool) {
